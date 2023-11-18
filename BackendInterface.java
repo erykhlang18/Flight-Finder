bb99295890051cfc85c19eab194c8673d7734f51
Aiden  Lang <ajlang5@wisc.edu>
2023-11-18 22:17:13 +0000

    Added initial frontend and backend interfaces

---
+++ BackendInterface.java
@@ -0,0 +1,32 @@
+/**
+ * Interface for backend functionality.
+ */
+public interface BackendInterface {
+
+    // Constructor comment: Expected constructor to accept a GraphADT implementation
+    // public IBackend(GraphADT graph) {
+    //   this.graph = graph; 
+   // }
+
+    /**
+     * Reads in graph data from file and stores it in a graph data structure.
+     * @param filePath the path to file
+     * @throws FileNotFoundException if it is the wrong file.
+     */
+    void readDataFile(String filePath)  throws FileNotFound;
+
+    /**
+     * Gets the shortest route from a start to a destination airport.
+     * @param start three letter code of the starting airport.
+     * @param end three letter code of the destination airport.
+     * @return an instance of ShortestPath.
+     */
+    ShortestPath getShortestRoute(String start, String end);
+
+    /**
+     * Gets stats about the graph dataset.
+     * return a String with stats including the number of airports,
+     * the number of flights, and the total miles of all edges.
+     */
+    String getGraphStats();
+}
